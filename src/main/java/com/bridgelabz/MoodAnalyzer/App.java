2ad93fff107833cfdde7e74e30c7f68595400e2c
phadalgekar <hadalgekarprathamesh@gmail.com>
2023-01-17 12:50:40 +0530

    adding welcome to mood analyzer

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer/App.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.MoodAnalyzer;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Mood Analyzer" );
+    }
+}

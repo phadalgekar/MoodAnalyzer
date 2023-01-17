b758ba3d57f31a0344e877197624f740e1a44b85
phadalgekar <hadalgekarprathamesh@gmail.com>
2023-01-17 13:15:10 +0530

    adding program to give message of happy or sad for mood analyzer

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.MoodAnalyzer;
+
+public class MoodAnalyzer {
+	public String analyseMood(String message) {
+
+        if (message.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
+
+
